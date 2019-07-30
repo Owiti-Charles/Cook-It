bd9eb48c6337fbd92f08e13df46de6784996ffa1
kamauvick <kamauvick@outlook.com>
2019-07-30 17:04:45 +0300

    Add a sliding carousel to the home page.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('.fullBackground').fullClip({
+        images: ['images/1.jpg', 'images/2.jpg', 'images/3.jpg'],
+        transitionTime: 2000,
+        wait: 5000
+    });
+})
