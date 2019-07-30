59cdf8d59ac8483112f9e4fd9d90a65e9f3e730a
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-07-30 16:52:10 +0300

    add transition functionality to the landing page.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+$('.fullBackground').fullClip({
+  images: ['images/chicken.jpg', 'images/yams.jpg', 'images/beans.jpg','images/spaghetti.jpg','images/avocado.jpg','images/spaghetti2.jpg','images/spicyrice.jpg' ],
+  transitionTime: 2000,
+  wait: 5000
+});
